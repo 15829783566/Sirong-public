cbd825c9bf411d2a462cc25dd42fae625ada29fe
15829783566 <40457064+15829783566@users.noreply.github.com>
2023-07-26 11:40:53 +0800

    Create 微信小程序自动打包上传.js

---
+++ 微信小程序自动打包上传.js
@@ -0,0 +1,31 @@
+/**
+ * 上传小程序代码
+ * 命令：node up 
+ */
+const ci = require('miniprogram-ci');
+(async () => {
+    const project = new ci.Project({
+        appid: 'wx2924301877de8d6d',
+        type: 'miniProgram',
+        projectPath: './dist/build/mp-weixin',
+        privateKeyPath: './private-v22.key',
+        ignores: ['node_modules/**/*'],
+    })
+    const uploadResult = await ci.upload({
+        project,
+        version: '1.0.0',
+        desc: '体验版-wang',
+        robot: 8,
+        setting: {
+            es6: true, // es6 转 es5
+            es7: true, // 增强编译
+            disableUseStrict: true,
+            autoPrefixWXSS: true, // 上传时样式自动补全
+            minifyJS: true,
+            minifyWXML: true,
+            minifyWXSS: true,
+        },
+    })
+    console.log(uploadResult)
+    console.log('发布成功！')
+})()
